58befc8df69d770c042d5d2f21cdf556497e4d3f
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-01-08 16:53:07 -0300

    Update quadrado.py

--- quadrado.py
+++ quadrado.py
@@ -1,3 +1,4 @@
+"""
 class quadrado:
     def __init__(self):
         self.altura = 2
@@ -19,3 +20,25 @@ class quadrado:
 
 square = Square()
 square.altura = 3  # gera AttributeError
+"""
+
+
+class quadrado:
+    def __init__(self, w, h):
+        self.altura = h
+        self.largura = w
+
+    def set_side(self, new_side):
+        self.altura = new_side
+        self.largura = new_side
+
+    @property
+    def altura(self):
+        return self.altura
+
+    @height.setter
+    def altura(self, new_value):
+        if new_value >= 0:
+            self.altura = new_value
+        else:
+            raise Exception("O valor precisa ser maior que zero")
